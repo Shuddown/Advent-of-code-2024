0c7d27de4c3974fa59c51a8127282e4216dea887
Shuddown <shudheshkibhakt@gmail.com>
2024-12-07 01:04:47 +0530

    First commit for setup

--- file-helper.sh
+++ file-helper.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+
+
+
 START=1
 END=25
 for day in $(seq $START $END); do
 
-    mkdir -p "Day$day"
-    touch  "Day$day/solution.go"
-    touch  "Day$day/solution.cpp"
-    touch  "Day$day/solution.lua"
+    mkdir -p "Day-$day"
+    touch  "Day-$day/solution.go"
+    touch  "Day-$day/solution.cpp"
+    touch  "Day-$day/solution.lua"
 done
 
 
